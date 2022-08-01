b777decd5da9dac92b606e59064dbf7b0eadb3a6
Robertoobarbosa <robertobarbosa2900@gmail.com>
2022-08-01 18:33:13 -0300

    Primeiro commit

--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -4,7 +4,17 @@ public class PrimeiroProgramaJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Gato gato = new Gato;
+		Livro livro = new Livro;
+		
+		System.out.println(gato);
+		System.out.println(livro);
 
 	}
-
+	
+	class Livro {
+		private String nome;
+		private Integer npag;
+	}
+	
 }
